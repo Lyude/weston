1611a45d5df04a043c6f5b4c3dcbd10ee74d20fa
Stephen Chandler Paul <thatslyude@gmail.com>
2014-08-08 01:19:26 -0400

    tablet: Move axis capabilities to tools
    Originally axis capabilities were part of the tablet, however different
    tools can report different axes, so we want the capabilities to be
    specific to each tool.

--- clients/window.c
+++ clients/window.c
@@ -147,6 +147,7 @@ struct tablet_tool {
 
 	enum wl_tablet_tool_type type;
 	uint32_t serial;
+	enum wl_tablet_tool_axis_flag axis_caps;
 
 	void *user_data;
 
@@ -3758,7 +3759,7 @@ tablet_manager_handle_device_added(void *data,
 				   struct wl_tablet_manager *wl_tablet_manager,
 				   struct wl_tablet *wl_tablet,
 				   const char *name, uint32_t vid, uint32_t pid,
-				   uint32_t type, uint32_t extra_axes)
+				   uint32_t type)
 {
 	struct input *input = data;
 	struct tablet *tablet = xzalloc(sizeof(*tablet));
@@ -3786,7 +3787,7 @@ tablet_manager_handle_tool_added(void *data,
 				 struct wl_tablet_manager *wl_tablet_manager,
 				 struct wl_tablet_tool *tool,
 				 enum wl_tablet_tool_type tool_type,
-				 uint32_t tool_serial)
+				 uint32_t tool_serial, uint32_t axis_caps)
 {
 	struct input *input = data;
 	struct tablet_tool *tablet_tool = xzalloc(sizeof(*tablet_tool));
@@ -3794,6 +3795,7 @@ tablet_manager_handle_tool_added(void *data,
 	*tablet_tool = (struct tablet_tool) {
 		.type = tool_type,
 		.serial = tool_serial,
+		.axis_caps = axis_caps,
 		.refcount = 1,
 		.wl_tablet_tool = tool,
 	};
